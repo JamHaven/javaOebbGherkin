17b19eefabc910c7f1d06e1b0808d99fe49fa78a
Mario Rubak <mario.rubak@stud.fh-campuswien.ac.at>
2020-03-14 10:15:55 +0100

    First steps working in Appium

--- src/test/java/oebbWeb/StepDefinition/TestTicketMobileSteps.java
+++ src/test/java/oebbWeb/StepDefinition/TestTicketMobileSteps.java
@@ -6,13 +6,22 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.nativekey.AndroidKey;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.remote.MobilePlatform;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.touch.TouchActions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 
@@ -34,25 +43,58 @@ public class TestTicketMobileSteps extends BaseSteps {
 
 		try {
 			driver = new AppiumDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+			//driver.findElementByXPath("(//android.widget.TextView[@content-desc=\"OK\"])[1]").click();
+			///driver.findElementByName("OK").click();
+			driver.findElementById("at.oebb.ts:id/header_addticket").click();
+			driver.findElementById("at.oebb.ts:id/header_addticket").click();
+			driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
+
 		//System.out.println(driver.getStatus());
     }		
 
-    @Given("^navigates to book a ticket$")
-	public void navigateToBooking(){
-		System.out.println("TODO: navigateToBooking");
-	}
+    @When("^Chooses one ticket$")
+    public void choosesOneTicket(DataTable dt) throws InterruptedException {
+		List<Map<String, String>> list = dt.asMaps(String.class, String.class);
+		for (Map<String, String> listMap: list) {
+			final String von = listMap.get("Von");
+			final String nach = listMap.get("Nach");
+			final String datum = listMap.get("Datum");
+			final String uhrzeit = listMap.get("Uhrzeit");
+			final String ab = listMap.get("Ab");
+			final String an = listMap.get("An");
+			/*driver.findElementById("at.oebb.ts:id/header_station_from").sendKeys(von);
+			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+			//driver.findElementByAccessibilityId(von).click();
+			driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.FrameLayout[5]/android.widget.LinearLayout/android.widget.ListView/android.widget.LinearLayout[2]/android.widget.LinearLayout/android.widget.ImageView[2]").click();
+			driver.findElementById("at.oebb.ts:id/header_station_to").sendKeys(nach);
+			driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+			driver.findElementByAccessibilityId(nach).click();
+			//driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.FrameLayout[5]/android.widget.LinearLayout/android.widget.ListView/android.widget.LinearLayout[1]/android.widget.LinearLayout/android.widget.ImageView[2]").click();
+			driver.findElementByAccessibilityId("Single Tickets and Day Tickets").click();*/
 
-    @When("^Chooses$")
-    public void sucheZugverbindung(DataTable dt)  {
-		System.out.println("TODO: sucheZugverbindung");
+
+			MobileElement el1 = (MobileElement) driver.findElementByAccessibilityId("departure city or station");
+			el1.sendKeys(von);
+			MobileElement el2 = (MobileElement) driver.findElementByAccessibilityId(von);
+			el2.click();
+			MobileElement el3 = (MobileElement) driver.findElementByAccessibilityId("arrival city or station");
+			el3.sendKeys(nach);
+			MobileElement el4 = (MobileElement) driver.findElementByAccessibilityId(nach);
+			el4.click();
+			MobileElement el5 = (MobileElement) driver.findElementByAccessibilityId("Single Tickets and Day Tickets");
+			el5.click();
+
+		}
+
     }		
 
-    @Then("^The ticket costs €\\d{1}.*")
+    @Then("^The ticket costs €14,00")
     public void ergebnislisteenthaelt(DataTable dt){
-		System.out.println("TODO: The ticket costs ...");
+		String prize = driver.findElementByAccessibilityId("€ 14,00").getText();
+		Assert.assertEquals("€ 14,00",prize);
     }
 
 }
--- src/test/resources/oebbWeb/TestMobileSuche.feature
+++ src/test/resources/oebbWeb/TestMobileSuche.feature
@@ -1,28 +1,19 @@
 Feature: Ticket price calculation in the OEBB Ticket Application on an Android device
-  Scenario: Buy one ticket from Vienna to St. Poelten
+  Scenario: Buy one ticket from Vienna to Wels
     Given The OEBB Ticket was started on an Android device and navigates to book a ticket
-    When Chooses
+    When Chooses one ticket
       |Von |Nach| Datum |Uhrzeit| Anzahl | Ermaessigung |
-      |Wien |St. Poelten| Montag in 3 Wochen | 9:00 |1| keine |
-      |St. Poelten |Wien| Montag in 3 Wochen | 9:00 |1| keine |
-      |Wien |St. Poelten | 20.11.2020        | 17:00|1| keine |
-      |St. Poelten |Wien | 20.11.2020        | 17:00|1| keine |
+      |Wien |Wels| 30.03.2020 | 9:00 |1| keine |
     Then The ticket costs €14,00
-  Scenario: Buy one ticket from Vienna to St. Poelten with a Vorteilscard
+  Scenario: Buy one ticket from Vienna to Wels with a Vorteilscard
     Given The OEBB Ticket was started on an Android device and navigates to book a ticket
-    When Chooses
+    When Chooses vorteilscard classic and one ticket
       |Von |Nach| Datum |Uhrzeit| Anzahl | Ermaessigung |
-      |Wien |St. Poelten| Montag in 3 Wochen | 9:00 |1| Vorteilscard Classic |
-      |St. Poelten |Wien| Montag in 3 Wochen | 9:00 |1| Vorteilscard Classic |
-      |Wien |St. Poelten | 20.11.2020        | 17:00|1| Vorteilscard Classic |
-      |St. Poelten |Wien | 20.11.2020        | 17:00|1| Vorteilscard Classic |
+      |Wien |Wels| Montag in 3 Wochen | 9:00 |1| Vorteilscard Classic |
     Then The ticket costs €5,90
-  Scenario: Buy four tickets from Vienna to St. Poelten
+  Scenario: Buy four tickets from Vienna to Wels
     Given The OEBB Ticket was started on an Android device and navigates to book a ticket
-    When Chooses
+    When Chooses four tickets
       |Von |Nach| Datum |Uhrzeit| Anzahl | Ermaessigung |
-      |Wien |St. Poelten| Montag in 3 Wochen | 9:00 |4| keine |
-      |St. Poelten |Wien| Montag in 3 Wochen | 9:00 |4| keine |
-      |Wien |St. Poelten | 20.11.2020        | 17:00|4| keine |
-      |St. Poelten |Wien | 20.11.2020        | 17:00|4| keine |
+      |Wien |Wels| Montag in 3 Wochen | 9:00 |4| keine |
     Then The ticket costs €45,20
